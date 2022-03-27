f80c67f848d2fb793b2e53ff77b8040e63dcd6fb
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-27 06:04:19 +0530

    added recursive knapsack

---
+++ Dynamic Programming/knapsackrec.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int knapsack(int wt[], int val[], int W, int n)
+{
+    if(W==0 || n == 0)
+        return 0;
+    if(wt[n-1]<=W)
+        return (max(val[n-1]+knapsack(wt,val,W-wt[n-1],n-1),knapsack(wt,val,W,n-1)));
+    else 
+        return knapsack(wt,val,W,n-1);
+}
+int main()
+{
+    int n,W;
+    cout <<"Enter the number of items :\n";
+    cin >> n;
+    int wt[n],val[n];
+    cout <<"Enter the weights:\n";
+    for(int i = 0;i<n;i++)
+     cin >> wt[i];
+    cout <<"Enter the values :\n";
+    for(int i = 0;i<n;i++)
+     cin >> val[i];
+    cout <<"Enter the weight carrying capacity of the knapsack: \n";
+    cin >> W;
+    cout << "The max profit is: " << knapsack(wt,val,W,n);
+
+    return 0;
+}
