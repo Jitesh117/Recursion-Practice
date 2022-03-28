ffafdaaff0176198d5ac477544812217fe1cb53b
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-28 07:55:01 +0530

    added count subset file

--- Dynamic Programming/countsubset.cpp
+++ Dynamic Programming/countsubset.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool IsSubsetsum(int arr[],int sum,int n)
+int countsubset(int arr[],int sum,int n)
 {
-    bool t[n+1][sum+1];
+    int t[n+1][sum+1];
     for(int i = 0;i<=n;i++)
         for(int j = 0;j<=sum;j++)
         {
             if( i == 0)
-             t[i][j] = false;
+             t[i][j] =0;
             if( j == 0)
-             t[i][j] = true;
+             t[i][j] = 1;
         }
     for(int i = 1;i<=n;i++)
      for(int j = 1;j<=sum;j++)
       {
           if(arr[i-1]<=j)
-                 t[i][j] = (t[i-1][j - arr[i-1]] || t[i-1][j]);
-                else
+                 t[i][j] = (t[i-1][j - arr[i-1]] + t[i-1][j]);
+          else
                  t[i][j] = t[i-1][j];
       }
     return t[n][sum];
@@ -34,18 +34,7 @@ int main()
     int sum;
     cout << "Enter the sum:\n";
     cin >> sum;
-    int total=0;
-    for(int i = 0;i<n;i++)
-        total+=arr[i];
-    if(total%2!=0)
-      cout << "False";
-    else
-    {
-    if(IsSubsetsum(arr,sum,n) == true)
-        cout <<"True";
-    else
-        cout << "False";
-    }
+    cout <<"Number of subsets of given sum:\n" << countsubset(arr,sum,n);
     
     return 0;
 }
