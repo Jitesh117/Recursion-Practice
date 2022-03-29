1c5a2b9459cd90d0c67b4539ca512ce13c8612fc
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-29 20:28:41 +0530

    practised permutation with case change again

--- PermCaseChange.cpp
+++ PermCaseChange.cpp
@@ -2,18 +2,17 @@
 using namespace std;
 void solve(string input,string output)
 {
-    if(input.size()==0)
-    {
-        cout<< output<< " ";
-        return;
-    }
-    string output1=output,output2=output;
-    output2.push_back(toupper(input[0]));
-    output1.push_back(input[0]);
-    input.erase(input.begin()+0);
-    solve(input,output1);
-    solve(input,output2);
-    return;
+   if(input.size()==0)
+   {
+       cout << output << " ";
+       return;
+   }
+   string output1 = output, output2 = output;
+   output2.push_back(input[0]);
+   output1.push_back(toupper(input[0]));
+   input.erase(input.begin()+0);
+   solve(input,output1);
+   solve(input,output2);
 }
 int main()
 {
