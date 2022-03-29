06fd2b53c765239edb33807d217c2f5047c883e4
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-29 05:53:07 +0530

    modified the memoized knapsack file

--- Dynamic Programming/memknapsack.cpp
+++ Dynamic Programming/memknapsack.cpp
@@ -5,6 +5,8 @@ int knapsack(int wt[], int val[], int W, int n)
 {
     if(W==0 || n ==0)
         return 0;
+    if(t[n][W]!=-1)
+        return t[n][W];
     if(wt[n-1]<=W)
     {
         t[n][W] = max(val[n-1]+ knapsack(wt,val,W-wt[n-1],n-1),knapsack(wt,val,W,n-1));
