1a0fbea08f26411c4dd6864eb13122f7da9e3ece
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-29 20:32:19 +0530

    practised permutation with spaces

--- permwithspace.cpp
+++ permwithspace.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 void solve(string ip,string op)
 {
-    if(ip.size()==0)
-        {
-            cout<< op << " ";
-            return;
-        }
-    string op2 = op,op1=op;
-    op2.push_back('-');
-    op2.push_back(ip[0]);
-    op1.push_back(ip[0]);
-
+   if(ip.size()==0)
+    {
+        cout << op << " ";
+        return;
+    }
+    string output1 = op, output2 = op;
+    output1.push_back('-');
+    output1.push_back(ip[0]);
+    output2.push_back(ip[0]);
     ip.erase(ip.begin()+0);
-    solve(ip,op1);
-    solve(ip,op2);
+    solve(ip,output1);
+    solve(ip,output2);
 
     return;
 }
