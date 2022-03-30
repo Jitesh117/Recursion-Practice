057732ee771d702085ab469a1280f3f558897065
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-30 21:15:25 +0530

    modified the permutation with space file

--- permwithspace.cpp
+++ permwithspace.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solve(string ip,string op)
+void solve(string input,string op)
 {
-   if(ip.size()==0)
+   if(input.size()==0)
     {
         cout << op << " ";
         return;
     }
     string output1 = op, output2 = op;
-    output1.push_back('-');
-    output1.push_back(ip[0]);
-    output2.push_back(ip[0]);
-    ip.erase(ip.begin()+0);
-    solve(ip,output1);
-    solve(ip,output2);
+    output1.push_back(input[0]);
+    output2.push_back(input[0]);
+    output2.push_back('_');
+    input.erase(input.begin()+0);
+    solve(input,output1);
+    solve(input,output2);
 
     return;
 }
