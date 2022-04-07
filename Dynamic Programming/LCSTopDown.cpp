bd6b0b9fe96e2c69aaee5bbc3eb3cf2b755c30e0
Jitesh117 <sahookumarjitesh@gmail.com>
2022-04-07 08:22:50 +0530

    modified top down LCS

--- Dynamic Programming/LCSTopDown.cpp
+++ Dynamic Programming/LCSTopDown.cpp
@@ -10,6 +10,10 @@ int main()
     for(int i = 0;i<=n;i++)
         for(int j = 0;j<=m;j++)
             t[i][j]=-1;
+    for(int i = 0;i<=n;i++)
+        for(int j = 0;j<=m;j++)
+          if(i == 0 || j == 0)
+            t[i][j]  = 0;
     for(int i = 1;i<=n;i++)
         for(int j= 1;j<=m;j++)
         {
@@ -21,6 +25,6 @@ int main()
                         t[i][j] = max(t[i-1][j],t[i][j-1]);
                 }
         }
-    cout << "LCS is:\n" << t[n][m]+1;
+    cout << "LCS is:\n" << t[n][m];
     return 0;
 }
