5f0cd200f856430f4cc2d20d6805f508ebd95f41
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-15 20:52:14 +0530

    added tabulation

--- DP striver/5MaxNonAdj.cpp
+++ DP striver/5MaxNonAdj.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int solve(int ind, vector<int> &nums)
+int solve(int ind, vector<int> &nums, vector<int> &dp) // memoization
 {
     if (ind == 0)
         return nums[ind];
     if (ind < 0)
         return 0;
-    int pick = solve(ind - 2, nums) + nums[ind];
-    int notpick = solve(ind - 1, nums);
-    return max(pick, notpick);
+    if (dp[ind] != -1)
+        return dp[ind];
+    int pick = solve(ind - 2, nums, dp) + nums[ind];
+    int notpick = solve(ind - 1, nums, dp);
+    return dp[ind] = max(pick, notpick);
 }
-int maxnonadj(vector<int> &nums)
+int maxnonadj(vector<int> &nums, vector<int> &dp)
 {
     int n = nums.size();
-    return solve(n - 1, nums);
+    return solve(n - 1, nums, dp);
 }
 int main()
 {
@@ -21,9 +23,22 @@ int main()
     int n;
     cin >> n;
     vector<int> nums(n);
+    // vector<int> dp(n, -1);
     cout << "Enter elements of array: ";
     for (int i = 0; i < n; i++)
         cin >> nums[i];
-    cout << "Max sum of non adjacent elements is: " << maxnonadj(nums);
+    // Tabulation
+    vector<int> dp(n);
+    dp[0] = nums[0];
+    for (int i = 1; i < n; i++)
+    {
+        int take = nums[i];
+        if (i > 1)
+            take += dp[i - 2];
+        int nontake = dp[i - 1];
+        dp[i] = max(take, nontake);
+    }
+    // cout << "Max sum of non adjacent elements is: " << maxnonadj(nums, dp);
+    cout << "Max sum of non adjacent elements is: " << dp[n-1];
     return 0;
 }
