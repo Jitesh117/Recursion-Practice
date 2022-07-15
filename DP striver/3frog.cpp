9f3211fc6cf0a6e28805dd009307872e6687c408
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-15 09:16:27 +0530

    added tabluation and space optimisation of frog jump

--- DP striver/3frog.cpp
+++ DP striver/3frog.cpp
@@ -13,3 +13,35 @@ int frog(int ind, vector<int> &heights, vector<int> dp)
 
     return dp[ind] = min(left, right);
 }
+// Tabulation
+int frogjump(int n, vector<int> &heights)
+{
+    vector<int> dp(n, 0);
+    for (int i = 1; i < n; i++)
+    {
+        int left = dp[i - 1] + abs(heights[i] - heights[i - 1]);
+        int right = INT_MAX;
+        if (i > 1)
+            right = dp[i - 2] + abs(heights[i] - heights[i - 2]);
+        dp[i] = min(left, right);
+    }
+    return dp[n - 1];
+}
+// Space Optimisation
+int fjump(int n, vector<int> &heights)
+{
+    int prev2 = 0;
+    int prev1 = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int left = prev2 + abs(heights[i] - heights[i - 1]);
+        int right = INT_MAX;
+        if (i > 1)
+            right = prev1 + abs(heights[i] - heights[i - 2]);
+
+        int curi = min(left, right);
+        prev2 = prev1;
+        prev1 = curi;
+    }
+    return prev1;
+}
