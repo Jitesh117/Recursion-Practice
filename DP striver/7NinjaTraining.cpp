2f766b76e9dcfc50e68d0a04e66e3d5e2d17c5ed
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-16 10:53:34 +0530

    added tabulation

--- DP striver/7NinjaTraining.cpp
+++ DP striver/7NinjaTraining.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Recursive
+
 int ninja(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp)
 {
     if (day == 0)
@@ -27,8 +30,41 @@ int ninja(int day, int last, vector<vector<int>> &points, vector<vector<int>> &d
     }
     return dp[day][last] = maxi;
 }
+
+// Memoization
+
 int ninjaTraining(int n, vector<vector<int>> &points)
 {
-    vector<vector<int>> dp(n, vector<int>(4, -1)); // memoization
+    vector<vector<int>> dp(n, vector<int>(4, -1));
     return ninja(n - 1, 3, points, dp);
+}
+
+// Tabulation
+
+int ninjaTraining(int n, vector<vector<int>> &v)
+{
+    vector<vector<int>> dp(n, vector<int>(4, 0));
+    dp[0][0] = max(v[0][1], v[0][2]);
+    dp[0][1] = max(v[0][0], v[0][2]);
+    dp[0][2] = max(v[0][1], v[0][0]);
+    dp[0][3] = max(v[0][1], max(v[0][0], v[0][2]));
+
+    //     int maxi = 0;
+    for (int day = 1; day < n; day++)
+    {
+        for (int last = 0; last < 4; last++)
+        {
+            dp[day][last] = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (i != last)
+                {
+                    int point = v[day][i] + dp[day - 1][i];
+                    dp[day][last] = max(dp[day][last], point);
+                }
+                //                 dp[day][last] = maxi;
+            }
+        }
+    }
+    return dp[n - 1][3];
 }
