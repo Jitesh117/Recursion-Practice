897bbe7cd884a71902c5da69eb474f606109149d
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-16 17:24:09 +0530

    added unique path with obstacles

---
+++ DP striver/9UniquePathObstacle.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solve(int m, int n, vector<vector<int>> &a)
+{
+    int dp[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i][j] == -1)
+                dp[i][j] = 0;
+            else if (i == 0 and j == 0)
+                dp[i][j] = 1;
+            else
+            {
+                int up = 0, left = 0;
+                if (i > 0)
+                    up = dp[i - 1][j];
+                if (j > 0)
+                    left = dp[i][j - 1];
+                dp[i][j] = up + left;
+            }
+        }
+    }
+    return dp[m - 1][n - 1];
+}
