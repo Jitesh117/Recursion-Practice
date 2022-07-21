2cb6a4d9c9e5d7c31568d12870e3daca878bfe93
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-21 09:11:47 +0530

    added minimum subset difference

---
+++ DP striver/16MinSubsetDiff.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int minSubsetSumDifference(vector<int> &arr, int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+    int k = sum;
+    vector<vector<bool>> dp(n, vector<bool>(k + 1, 0));
+    for (int i = 0; i < n; i++)
+        dp[i][0] = true;
+    dp[0][arr[0]] = true;
+
+    for (int ind = 1; ind < n; ind++)
+    {
+        for (int target = 1; target <= k; target++)
+        {
+            bool nontake = dp[ind - 1][target];
+            bool take = false;
+            if (arr[ind] <= target)
+                take = dp[ind - 1][target - arr[ind]];
+            dp[ind][target] = take | nontake;
+        }
+    }
+    int mini = 1e9;
+    for (int s1 = 0; s1 <= sum; s1++)
+    {
+        if (dp[n - 1][s1])
+            mini = min(mini, abs((sum - s1) - s1));
+    }
+    return mini;
+}
